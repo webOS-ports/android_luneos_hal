9ff775798b871a4414968c80875867bed43227fa
Tofe <chris.chapuis@gmail.com>
2017-09-14 08:56:08 +0200

    Apply_patches: abort previous git am in case of failure

--- patches/apply_patches.sh
+++ patches/apply_patches.sh
@@ -9,6 +9,7 @@ for dir in $(find . -mindepth 1 -type d -links 2); do
     for patch in $(find ${dir} -mindepth 1 -type f -name "*.patch" | sort); do
       echo " --> Applying $patch"
       cd ../../../$(dirname $patch)
+      git am --abort
       git am ${SCRIPT_DIR}/${patch}
       cd ${SCRIPT_DIR}
     done  
